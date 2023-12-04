eece73ef2b87ab86b505aa66a4b5fd0364688353
manuelmsni <manuelmsni@hotmail.com>
2023-12-04 21:36:03 +0100

    Cambiada la versión de la SDK a la 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.timemap"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.timemap"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
