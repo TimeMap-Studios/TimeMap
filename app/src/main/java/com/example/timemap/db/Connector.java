a97e0c5db1814b0876b468f37938cd3d927ae59b
slimm1 <tini.ramonda@gmail.com>
2024-01-23 00:09:17 +0100

    added db classes... test model data pending.

--- app/src/main/java/com/example/timemap/db/Connector.java
+++
@@ -1,5 +0,0 @@
-package com.example.timemap.db;
-
-public class Connector {
-
-}
---
+++ app/src/main/java/com/example/timemap/db/DatabaseHelper.java
@@ -0,0 +1,97 @@
+package com.example.timemap.db;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "timemap.db";
+    public static String DATABASE_PATH = "";
+    private SQLiteDatabase timemapDB;
+    private Context context;
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+        this.context = context;
+        this.DATABASE_PATH = "/data/data/"+context.getPackageName()+"/databases/";
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @Override
+    public synchronized void close() {
+        if(timemapDB!=null){
+            timemapDB.close();
+        }
+        super.close();
+    }
+    /***
+     * Check if the database exists on device or not
+     */
+    private boolean checkDataBase() {
+        SQLiteDatabase tempDB = null;
+        try {
+            String myPath = DATABASE_PATH + DATABASE_NAME;
+            tempDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+        } catch (SQLiteException e) {
+            Log.e("timemap - check", e.getMessage());
+        }
+        if (tempDB != null)
+            tempDB.close();
+        return tempDB != null ? true : false;
+    }
+    public void copyDataBase() throws IOException {
+        try {
+            InputStream myInput = context.getAssets().open(DATABASE_NAME);
+            String outputFileName = DATABASE_PATH + DATABASE_NAME;
+            OutputStream myOutput = new FileOutputStream(outputFileName);
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while((length = myInput.read(buffer))>0){
+                myOutput.write(buffer, 0, length);
+            }
+
+            myOutput.flush();
+            myOutput.close();
+            myInput.close();
+        } catch (Exception e) {
+            Log.e("timemap - copyDatabase", e.getMessage());
+        }
+    }
+    public void openDataBase() throws SQLException {
+        String myPath = DATABASE_PATH + DATABASE_NAME;
+        timemapDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+    public void createDataBase() throws IOException {
+        boolean dbExist = checkDataBase();
+
+        if (dbExist) {
+
+        } else {
+            this.getReadableDatabase();
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                Log.e("timemap - create", e.getMessage());
+            }
+        }
+    }
+}
